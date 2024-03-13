4d80d88e04160be03e42b97ef4a3d771202e7370
Dana Ciocan <danaciocan@economist.com>
2024-03-13 21:32:15 +0000

    Put NEXTAUTH_URL in .env file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    NEXTAUTH_URL: 'http://localhost:3000',
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
