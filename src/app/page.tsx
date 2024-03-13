648d185e8cbb15f6d04af4cf62ebc31ee16675b1
Dana Ciocan <danaciocan@economist.com>
2024-03-13 18:07:39 +0000

    Display welcome banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
-import styles from './page.module.css';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/app/lib/auth';
+import { WelcomeBanner } from '@/components/welcome-banner/WelcomeBanner';
 
-export default function Home() {
-  return <main className={styles.main}></main>;
+export default async function Home() {
+  // Get user session token
+  const session = await getServerSession(authOptions);
+  // session = null || { user: { name, email, image } }
+
+  return (
+    <div>
+      {session && (
+        <div>
+          <WelcomeBanner
+            name={session.user?.name?.split(' ')[0] || 'there'}
+            team='some-team'
+          />
+          <a href='/api/auth/signout'>Sign out</a>
+        </div>
+      )}
+
+      {!session && <p>Not signed in</p>}
+    </div>
+  );
 }
