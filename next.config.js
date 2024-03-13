426dda92229cdc4188727fb5a5227d5e5777062b
Dana Ciocan <danaciocan@economist.com>
2024-03-13 18:07:04 +0000

    Add env variables to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+    ],
+  },
+  env: {
+    NEXTAUTH_URL: 'http://localhost:3000',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
