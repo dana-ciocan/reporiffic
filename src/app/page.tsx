b70912e368b0cb122e98b8ead47a305f01398647
Dana Ciocan <danaciocan@economist.com>
2024-11-06 10:51:53 +0000

    Allow users to log in/out using links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,26 @@ import { WelcomeBanner } from '@/components/welcome-banner/WelcomeBanner';
 import { TopMenu } from '@/components/top-menu/TopMenu';
 
 export default async function Home() {
-  // Get user session token
   const session = await getServerSession(authOptions);
-  // session = null || { user: { name, email, image } }
 
   return (
     <div>
-      {!session && <p>Not signed in</p>}
-      {session && (
-        <div>
-          <TopMenu />
-
-          <WelcomeBanner name={session.user?.name?.split(' ')[0]} />
-        </div>
-      )}
+      <TopMenu
+        loggedIn={session?.user !== undefined}
+        avatar={session?.user?.image || undefined}
+      />
+      <WelcomeBanner name={(session?.user?.name || 'there').split(' ')[0]} />
+      <main>
+        {session ? (
+          <>Here are your repos:</>
+        ) : (
+          <>
+            You are not signed in -{' '}
+            <a href='/api/auth/signin'>sign in with your Github account</a> to
+            see all your repos and start tracking your progress!
+          </>
+        )}
+      </main>
     </div>
   );
 }
