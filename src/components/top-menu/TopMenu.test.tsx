c1fe596b2ca2675bb1c973f04aa665bdd4df7bb9
Dana Ciocan <danaciocan@economist.com>
2024-11-06 10:51:39 +0000

    Flesh out the top menu

--- src/components/top-menu/TopMenu.test.tsx
+++ src/components/top-menu/TopMenu.test.tsx
@@ -3,9 +3,26 @@ import { render, screen } from '@testing-library/react';
 import { TopMenu } from './TopMenu';
 
 describe('<TopMenu>', () => {
-  it('should render a log out link', () => {
-    render(<TopMenu />);
+  it('should render a sign out link when the user is logged in', () => {
+    render(<TopMenu loggedIn={true} />);
 
     expect(screen.getByRole('link', { name: 'Sign out' })).toBeInTheDocument();
+  });
+
+  it('should render a sign in link when the user is logged out', () => {
+    render(<TopMenu loggedIn={false} />);
+
+    expect(screen.getByRole('link', { name: 'Sign in' })).toBeInTheDocument();
+  });
+
+  it('should render an avatar if an image URL is provided and the user is logged in', () => {
+    render(
+      <TopMenu
+        loggedIn={true}
+        avatar='https://cdn.somecdn.com/images/avatar.png'
+      />,
+    );
+
+    expect(screen.getByRole('img')).toBeInTheDocument();
   });
 });
--- src/components/top-menu/TopMenu.tsx
+++ src/components/top-menu/TopMenu.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import styles from './TopMenu.module.css';
+import Image from 'next/image';
 
-export const TopMenu = () => (
+export interface TopMenuProps {
+  loggedIn: boolean;
+  avatar?: string;
+}
+
+export const TopMenu = ({ loggedIn, avatar }: TopMenuProps): JSX.Element => (
   <nav className={styles['top-menu']}>
-    <a href='/api/auth/signout'>Sign out</a>
+    {loggedIn ? (
+      <a href='/api/auth/signout'>Sign out</a>
+    ) : (
+      <a href='/api/auth/signin'>Sign in</a>
+    )}
+    {avatar && (
+      <Image
+        className={styles['avatar']}
+        width={100}
+        height={100}
+        src={avatar}
+        alt=''
+      />
+    )}
   </nav>
 );
